4447104ae6f13a479aa0b16abd09d5090d8e60f3
Jeff Perrin <jeff@jeffperrin.com>
2009-07-09 12:35:51 -0600

    Did some docs, made object_mother look for factories in rails test dir

--- lib/object_mother.rb
+++ lib/object_mother.rb
@@ -1,37 +1,10 @@
-module ObjectMother
-  class Factory
-    @@index = 0
-    def self.spawn
-      raise 'No create method specified'
-    end
-    def self.populate(model)
-      raise 'No populate method specified for ' + model.to_s
-    end
-    def self.to_hash(model)
-      raise 'No to_hash method specified'
-    end
-    def self.unique(val)
-      @@index += 1
-      val.to_s + @@index.to_s
-    end
-    def self.unique_email(val)
-      user, domain = val.split('@')
-      unique(user) + '@' + domain
-    end
-    def self.create
-      obj = spawn
-      populate(obj)
-      yield obj if block_given?
-      obj
-    end
-    def self.create!(&block)
-      obj = create(&block)
-      obj.save!
-      obj
-    end
-    def self.create_as_hash(&block)
-      obj = create(&block)
-      to_hash obj
+require 'object_mother/factory'
+
+if defined? Rails.configuration
+  Rails.configuration.after_initialize do
+    path = File.join(RAILS_ROOT, 'test', 'object_mother')
+    Dir[File.join(path, '*.rb')].each do |file|
+      require file
     end
   end
 end
